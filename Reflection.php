bca033b21ca5e9ee221b2deda0dec408bf5f333d
Fano0612 <88125494+Fano0612@users.noreply.github.com>
2022-03-22 16:43:23 +0700

    Update Reflection.php

--- Reflection.php
+++ Reflection.php
@@ -1,3 +1,5 @@
+<!-- Sebuah institusi ingin melakukan pendataan identitas peserta yang ada dengan menggunakan suatu program bernama Identity, yang mana program ini menggunakan konsep Reflection dalam PHP. Program identity menerima input nama, umur, alamat/asal, dan hobi, kemudian mengeluarkan hasil inputan tersebut. -->
+
 <?php
 
 class Identity {
